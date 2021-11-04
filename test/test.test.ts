f59c35931cc75f278369c7bc0fb23f290d34c4ab
Michał Grygierzec <michal.grygierzec@protonmail.com>
2021-11-04 21:33:44 +0000

    Configure testing env with Jest

---
+++ test/test.test.ts
@@ -0,0 +1,10 @@
+import * as T from "@effect-ts/core/Effect"
+import * as Ex from "@effect-ts/core/Effect/Exit"
+
+describe("Mars Kata", () => {
+  describe("Sol 1", () => {
+    it("should work", () => {
+        expect(true).toBe(true);
+    });
+  });
+});
