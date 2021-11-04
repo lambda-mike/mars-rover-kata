08038e78e093dcc447c3c36d45df744f0e764765
Michał Grygierzec <michal.grygierzec@protonmail.com>
2021-11-04 21:03:37 +0000

    add nix

---
+++ shell.nix
@@ -0,0 +1,21 @@
+let
+
+  sources = import ./nix/sources.nix;
+
+  nixpkgs = sources.nixpkgs;
+
+  pkgs = import nixpkgs {};
+
+  niv = (import sources.niv {}).niv;
+
+in pkgs.mkShell rec {
+
+  name = "Mars-Rover-Kata";
+
+  buildInputs = [
+    niv
+    #pkgs.nodejs-16_x
+    #pkgs.pnpm
+  ];
+
+}
