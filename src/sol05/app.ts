8567e11c8014418d957b4f0be0f84012486a1157
Michał Grygierzec <michal.grygierzec@protonmail.com>
2021-11-25 22:54:50 +0000

    Sol05 draft of app

--- src/sol05/app.ts
+++ src/sol05/app.ts
@@ -1,4 +1,3 @@
-import * as readline from "node:readline"
 import * as process from "node:process"
 import * as As from "@effect-ts/core/Async"
 import { pipe } from "@effect-ts/core/Function"
@@ -7,36 +6,53 @@ import {
     readFile,
     logger,
 } from "./infra";
+import {
+    Environment,
+} from "./domain";
 
-const main = (): Promise<void> => pipe(
-    As.gen(function*(_) {
-        yield* _(logger.log("Welcome to Mars, Rover!"));
-        const cmds = yield* _(readConsole("Please, enter commands for the Rover in 'F,B,R,L' format: "));
-        yield* _(logger.log(`Rover is executing commands: ${cmds}`));
-        // TODO program
-    }),
-    As.foldM(
-        (err) => logger.error("Mission failed:", err),
-        () => logger.log("Mission completed!"),
-    ),
-    As.runPromise,
-);
+const app = As.gen(function*(_) {
+    const planetFile = yield* _(As.access((env: { planetFile: string }) => env.planetFile));
+    const roverFile = yield* _(As.access((env: { roverFile: string }) => env.roverFile));
+
+    const planetStr = yield* _(readFile(planetFile));
+    const roverStr = yield* _(readFile(roverFile));
+
+    // TODO remove after debugging
+    yield* _(logger.log("[DBG]", planetStr, roverStr));
+
+    yield* _(logger.log("Welcome to Mars, Rover!"));
+    const cmds = yield* _(readConsole("Please, enter commands for the Rover in 'F,B,R,L' format: "));
+
+    // TODO replace with travel and outputting the result
+    yield* _(logger.log(`Rover is executing commands: ${cmds}`));
+});
+
+const main = (): Promise<void> => {
+    const env: Environment = {
+        planetFile: "planet.txt",
+        roverFile: "rover.txt",
+    };
+    return pipe(
+        app,
+        As.foldM(
+            (err) => logger.error("Mission failed:", err),
+            () => logger.log("Mission completed!"),
+        ),
+        As.catchAll(
+            (err) => logger.error("Unexpected captured error: ", err)
+        ),
+        As.provideAll(env),
+        // TODO replace with runPromiseExit
+        As.runPromise,
+    );
+};
 
 (async () => {
-    await main();
-    process.exit(0);
+    try {
+        await main();
+    } catch (err) {
+        console.error("Unexpected error captured: ", err)
+    } finally {
+        process.exit(0);
+    }
 })()
-
-
-// const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// });
-
-// rl.question('What do you think of Node.js? ', (answer: string) => {
-//     // TODO: Log the answer in a database
-//     console.log(`Thank you for your valuable feedback: ${answer}`);
-
-//     rl.close();
-//     process.exit(0);
-// });
