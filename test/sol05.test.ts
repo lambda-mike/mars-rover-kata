8ca8058d45083c063a778696315a862d31d9bc95
Michał Grygierzec <michal.grygierzec@protonmail.com>
2021-11-27 21:03:55 +0000

    clean sol05 test

--- test/sol05.test.ts
+++ test/sol05.test.ts
@@ -1,6 +1,4 @@
-import * as path from "node:path"
 import * as As from "@effect-ts/core/Async"
-import * as Ex from "@effect-ts/system/Exit"
 import * as E from "@effect-ts/core/Either"
 import { pipe } from "@effect-ts/core/Function"
 import {
@@ -28,7 +26,6 @@ import {
   travel,
 } from "../src/sol05/domain";
 import {
-  readConsole,
   readFile,
 } from "../src/sol05/infra";
 
