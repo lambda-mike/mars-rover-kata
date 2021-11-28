f081ec4e0cd2b9e6b41258df649d14d9fcbea1e4
Michał Grygierzec <michal.grygierzec@protonmail.com>
2021-11-28 15:07:04 +0000

    Sol07 init

---
+++ src/sol07/infra.ts
@@ -0,0 +1,71 @@
+import * as fs from "node:fs"
+import * as readline from "node:readline"
+import * as process from "node:process"
+import * as As from "@effect-ts/core/Async"
+import {
+    Logger,
+    ReadConsoleError,
+    ReadFileError,
+} from "./domain";
+
+// TODO accept Logging Service replace console.log with service
+export const readFile = (filename: string): As.IO<ReadFileError, string> =>
+    As.promise(
+        () => new Promise((resolve, reject) => {
+            fs.readFile(filename, 'utf8', (err, data) => {
+                if (err) {
+                    //console.error("[readFile]", err, JSON.stringify(err));
+                    return reject(err);
+                }
+                return resolve(data.trim());
+            });
+        }),
+        // TODO log error
+        (error): ReadFileError => ({
+            kind: "ReadFileError",
+            filename,
+            error,
+        }),
+    );
+
+export const readConsole =
+    (prompt: string): As.IO<ReadConsoleError, string> =>
+        As.promise(
+            () => new Promise((resolve, reject) => {
+                const rl = readline.createInterface({
+                    input: process.stdin,
+                    output: process.stdout,
+                });
+                rl.question(prompt, (answer: string) => {
+                    // fatal error simulation
+                    if (answer.includes('X')) reject('X');
+                    rl.close();
+                    return resolve(answer);
+                });
+            }),
+            (err): ReadConsoleError => ({
+                kind: "ReadConsoleError",
+                error: err,
+            }));
+
+export const writeConsole =
+    (s: string): As.UIO<void> =>
+        As.succeedWith(() => console.log(s));
+
+const log =
+    (...args: unknown[]): As.UIO<void> =>
+        As.succeedWith(() => console.log("[LOG]", ...args));
+
+const error =
+    (...args: unknown[]): As.UIO<void> =>
+        As.succeedWith(() => console.error("[ERR]", ...args));
+
+const warn =
+    (...args: unknown[]): As.UIO<void> =>
+        As.succeedWith(() => console.warn("[WARN]", ...args));
+
+export const getLogger = (): Logger => ({
+    error,
+    log,
+    warn,
+});
