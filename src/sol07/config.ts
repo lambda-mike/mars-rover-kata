246cacd925329e847de863b224e70cc533a23190
Michał Grygierzec <michal.grygierzec@protonmail.com>
2021-11-30 22:02:35 +0000

    Sol07 simplify Config service

--- src/sol07/config.ts
+++ src/sol07/config.ts
@@ -1,18 +1,9 @@
-import * as L from "@effect-ts/core/Effect/Layer"
-import * as M from "@effect-ts/core/Effect/Managed"
 import { tag } from "@effect-ts/core/Has"
-import type { _A } from "@effect-ts/core/Utils"
 
-export const makeConfigLive = M.succeedWith(() => {
-    return {
-        _tag: "Config",
-        planetFile: "planet.txt",
-        roverFile: "rover.txt",
-    };
-});
-
-export interface Config extends _A<typeof makeConfigLive> { }
+export interface Config {
+    readonly _tag: "Config";
+    readonly planetFile: string;
+    readonly roverFile: string;
+}
 
 export const Config = tag<Config>();
-
-export const ConfigLive = L.fromManaged(Config)(makeConfigLive);
